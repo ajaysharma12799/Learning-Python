9cc5be2924911d549c7deb84fcf2e19f838b2c70
ajaysharma12799 <ajaysharma12799@gmail.com>
2020-08-20 02:35:48 +0530

    worked on list comprehension

--- Input-Output/multipleInputTaking.py
+++ Input-Output/multipleInputTaking.py
@@ -3,6 +3,17 @@
     1. using String split() method
     2. using list comprehension
 """
-name, age, marks = input("Enter Your Name, Age and Marks : ").split()
 
+# Using Split Method
+name, age, marks = input("Enter Your Name, Age and Marks : ").split()
 print("Name : {}, Age: {} and Marks : {}".format(name, age,marks))
+
+# Using List Comprehension
+numbers = [1, 2, 3, 4, 5] 
+square = [ n**2 for n in numbers ] # finding square of each number
+print(square)
+
+list1 = [ 1,3,2,5,4 ]
+list2 = [ 7,6,1,3,9 ]
+equalElement = [ i for i in list1 for j in list2 if i == j ] # finding if both list contain equal element or not
+print(equalElement)
