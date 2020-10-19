50b253bc0c771335a74dc0d62c3154d9abea9ddc
ajaysharma12799 <ajaysharma12799@gmail.com>
2020-10-20 02:54:13 +0530

    learned about os module

--- Standard Library/app.py
+++ Standard Library/app.py
@@ -11,3 +11,7 @@ print(os.getcwd()) # Get Current Working Directory
 print(os.name) # Print OS Name
 print(os.environ) # Return Dictionary of user's Environment Variable
 print(os.chdir(r"C:\Users\Ajay\Desktop")) # Used TO Change Working Directory
+
+print( dir(os) ) # Return Sorted List of String, List Contain Name of variable and function defined in module
+print( globals() ) # Return Name of Function that can be accessed globally from that function
+print( locals() ) # Return Name of Function that can be accessed locally from that function
